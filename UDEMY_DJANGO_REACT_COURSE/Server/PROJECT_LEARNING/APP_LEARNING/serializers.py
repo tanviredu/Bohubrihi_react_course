7bcf50ad5ea16255fdc421957b3f1f0d63eeebce
tanvir_bespoke <tanvir.bespoke@gmail.com>
2022-09-25 16:21:49 +0600

    added

--- UDEMY_DJANGO_REACT_COURSE/Server/PROJECT_LEARNING/APP_LEARNING/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['title','descrption','price','published','is_published','cover']
